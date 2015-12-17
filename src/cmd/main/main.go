023a7082f3db86ef9fe523b0a0267d20f1b61d53
weisd <weishidavip@163.com>
2015-12-17 17:07:16 +0800

    add config

--- src/cmd/main/main.go
+++ src/cmd/main/main.go
@@ -93,7 +93,7 @@ func gitPullSrc(path string) (string, error) {
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "his")
+	fmt.Fprintf(w, "hi man")
 }
 
 var tomlPath string
